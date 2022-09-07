6a2d0560faa5f735bc41061f9636559f94f7ef90
Your Name <107790440+daikingkong@users.noreply.github.com>
2022-09-07 15:02:34 +0000

    [Add]モデルのアソシエーションとバリデーションの記述追加

--- app/models/memory_comment.rb
+++ app/models/memory_comment.rb
@@ -1,2 +1,7 @@
 class MemoryComment < ApplicationRecord
+  belongs_to :end_user
+  belongs_to :memory
+
+  validates :comment, presence: true
+
 end
--- app/models/memory_search_tag.rb
+++ app/models/memory_search_tag.rb
@@ -1,2 +1,5 @@
 class MemorySearchTag < ApplicationRecord
+  belongs_to :memory
+  belongs_to :memory_tag
+
 end
