46406e7b02611b7287a73f0251416ca1cb06f9e2
Your Name <107790440+daikingkong@users.noreply.github.com>
2022-09-08 13:29:44 +0000

    [Add]ユーザーマイページのユーザー編集画面リンク、マイグループ一覧（各グループページリンク付き）、グループ作成画面リンク、メモリー新規投稿画面リンク、マイメモリー一覧（各メモリー詳細画面リンク付き）、ページネーション追加

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -4,6 +4,7 @@ class Public::EndUsersController < ApplicationController
   def show
     @end_user = current_end_user
     @end_user_groups = @end_user.groups
+    @end_user_memories = @end_user.memories.page(params[:page]).per(6)
   end
 
   def edit
