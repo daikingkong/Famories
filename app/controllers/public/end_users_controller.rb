e8815b0d8e08acc82447ff898e535b0b69985485
Your Name <107790440+daikingkong@users.noreply.github.com>
2022-09-08 14:33:49 +0000

    [Add]会員の情報編集画面のフォームとボタンとリンクまで作成

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -2,12 +2,13 @@ class Public::EndUsersController < ApplicationController
   layout "public_application"
 
   def show
-    @end_user = current_end_user
+    @end_user = EndUser.find(params[:id])
     @end_user_groups = @end_user.groups
     @end_user_memories = @end_user.memories.page(params[:page]).per(6)
   end
 
   def edit
+    @end_user = EndUser.find(params[:id])
   end
 
   def updata
