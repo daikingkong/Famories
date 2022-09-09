954ebfd2aab1798fd5e4546189719f3601c55b30
Your Name <107790440+daikingkong@users.noreply.github.com>
2022-09-09 02:11:26 +0000

    [Add]メモリ―詳細ページのコメント投稿機能と削除機能追加

--- app/controllers/public/memory_comments_controller.rb
+++ app/controllers/public/memory_comments_controller.rb
@@ -1,9 +1,28 @@
 class Public::MemoryCommentsController < ApplicationController
 
   def create
+    @memory = Memory.find(params[:memory_id])
+    memory_comment = MemoryComment.new(memory_commnet_params)
+    memory_comment.end_user_id = current_end_user.id
+    memory_comment.memory_id = @memory.id
+    if memory_comment.save
+      redirect_to memory_path(@memory), notice: "コメントを投稿しました。"
+    else
+      redirect_to memory_path(@memory), notice: "コメントの投稿に失敗しました。"
+    end
   end
 
   def destroy
+    @memory = Memory.find(params[:id])
+    memorycomment = MemoryComment.find(params[:memory_id])
+    memorycomment.destroy
+    redirect_to memory_path(@memory),  notice: "コメントを削除しました。"
+  end
+
+  private
+
+  def memory_commnet_params
+    params.require(:memory_comment).permit(:comment, :memory_id, :end_user_id)
   end
 
 end
