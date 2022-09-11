5a1814b8f6f3f1dcad39f8fc6bafb4d46d342fe1
Your Name <107790440+daikingkong@users.noreply.github.com>
2022-09-11 03:20:38 +0000

    [Add]グループ加入リクエスト解除機能

--- app/controllers/public/join_requests_controller.rb
+++ app/controllers/public/join_requests_controller.rb
@@ -1,6 +1,9 @@
 class Public::JoinRequestsController < ApplicationController
   layout "public_application"
 
+  def index
+  end
+
   def create
     group = Group.find(params[:group_id])
     join_request = current_end_user.join_requests.new(group_id: group.id, end_user_id: current_end_user.id)
@@ -9,9 +12,10 @@ class Public::JoinRequestsController < ApplicationController
   end
 
   def destroy
-  end
-
-  def index
+    group = Group.find(params[:group_id])
+    join_request = current_end_user.join_requests.find_by(group_id: group.id, end_user_id: current_end_user.id)
+    join_request.destroy
+    redirect_to request.referer
   end
 
 end
