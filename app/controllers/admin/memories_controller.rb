9d47d652cacb14013bb36b0cc574f59117a66d78
Your Name <107790440+daikingkong@users.noreply.github.com>
2022-09-13 06:04:24 +0000

    [Add]メモリー詳細ページの作成、削除機能追加

--- app/controllers/admin/memories_controller.rb
+++ app/controllers/admin/memories_controller.rb
@@ -2,9 +2,15 @@ class Admin::MemoriesController < ApplicationController
   layout "admin_application"
 
   def show
+    @memory = Memory.find(params[:id])
+    @memory_tags = @memory.memory_tags.page(params[:page]).per(5)
+    @memory_comments = @memory.memory_comments.page(params[:page]).per(8)
   end
 
   def destroy
+    memory = Memory.find(params[:id])
+    memory.destroy
+    redirect_to admin_end_user_path(memory.end_user_id)
   end
 
 end
