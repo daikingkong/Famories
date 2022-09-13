8c2aa450b16ba40c37db74c8a7d6cf429d8f5887
Your Name <107790440+daikingkong@users.noreply.github.com>
2022-09-13 07:08:58 +0000

    [Add]管理者側のグループ削除機能追加

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -12,6 +12,9 @@ class Admin::GroupsController < ApplicationController
   end
 
   def destroy
+    group = Group.find(params[:id])
+    group.destroy
+    redirect_to admin_groups_path
   end
 
 end
