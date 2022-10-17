0e277a1c3bbd064e747ae1483beb1b12ea5a4da5
Your Name <107790440+daikingkong@users.noreply.github.com>
2022-10-17 05:38:56 +0000

    [Add]メモリーのコメント非同期化

--- app/controllers/public/memory_comments_controller.rb
+++ app/controllers/public/memory_comments_controller.rb
@@ -3,27 +3,25 @@ class Public::MemoryCommentsController < ApplicationController
   before_action :ensure_guest_user
 
   def create
-    memory = Memory.find(params[:memory_id])
-    memory_comment = MemoryComment.new(memory_commnet_params)
-    memory_comment.end_user_id = current_end_user.id
-    memory_comment.memory_id = memory.id
-    if memory_comment.save
-      redirect_to memory_path(memory), notice: "コメントを投稿しました。"
-    else
-      redirect_to memory_path(memory), alert: "コメントの投稿に失敗しました。"
-    end
+    @memory = Memory.find(params[:memory_id])
+    @memory_comments = @memory.memory_comments.page(params[:page]).per(10).order("created_at DESC")
+    @memory_comment = MemoryComment.new(memory_comment_params)
+    @memory_comment.end_user_id = current_end_user.id
+    @memory_comment.memory_id = @memory.id
+    @memory_comment.save
   end
 
   def destroy
-    memory = Memory.find(params[:id])
-    memorycomment = MemoryComment.find(params[:memory_id])
-    memorycomment.destroy
-    redirect_to memory_path(memory),  notice: "コメントを削除しました。"
+    @memory = Memory.find(params[:memory_id])
+    @memory_comments = @memory.memory_comments.page(params[:page]).per(10).order("created_at DESC")
+    @memorycomment = MemoryComment.find(params[:id])
+    @memorycomment.destroy
+    # redirect_to memory_path(@memory),  notice: "コメントを削除しました。"
   end
 
   private
 
-  def memory_commnet_params
+  def memory_comment_params
     params.require(:memory_comment).permit(:comment, :memory_id, :end_user_id)
   end
 
