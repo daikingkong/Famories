faa1943807dfe894b97ceb04a5e91a6486b02493
Your Name <107790440+daikingkong@users.noreply.github.com>
2022-10-17 01:02:30 +0000

    [Add]メモリーのいいね非同期化

--- app/controllers/public/memory_favorites_controller.rb
+++ app/controllers/public/memory_favorites_controller.rb
@@ -2,17 +2,15 @@ class Public::MemoryFavoritesController < ApplicationController
   before_action :authenticate_end_user!
 
   def create
-    memory = Memory.find(params[:memory_id])
-    memory_favorite = current_end_user.memory_favorites.new(memory_id: memory.id)
-    memory_favorite.save
-    redirect_to memory_path(memory)
+    @memory = Memory.find(params[:memory_id])
+    @memory_favorite = current_end_user.memory_favorites.new(memory_id: @memory.id)
+    @memory_favorite.save
   end
 
   def destroy
-    memory = Memory.find(params[:memory_id])
-    memory_favorite = current_end_user.memory_favorites.find_by(memory_id: memory.id)
-    memory_favorite.destroy
-    redirect_to memory_path(memory)
+    @memory = Memory.find(params[:memory_id])
+    @memory_favorite = current_end_user.memory_favorites.find_by(memory_id: @memory.id)
+    @memory_favorite.destroy
   end
 
 end
