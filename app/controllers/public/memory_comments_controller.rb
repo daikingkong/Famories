0995030b39d0b048d29380fffa9c7df46e66ffd8
Your Name <107790440+daikingkong@users.noreply.github.com>
2022-10-25 04:57:54 +0000

    [Add]メモリー詳細のコメント一覧の削除はログインユーザー以外できないようにした

--- app/controllers/public/memory_comments_controller.rb
+++ app/controllers/public/memory_comments_controller.rb
@@ -9,22 +9,21 @@ class Public::MemoryCommentsController < ApplicationController
     @memory_comment.end_user_id = current_end_user.id
     @memory_comment.memory_id = @memory.id
     @memory_comment.save
-    # if @memory_comment.save
-      # redirect_to memory_path(@memory), notice: "コメントを投稿しました。"
-    # else
-    #   # redirect_to memory_path(@memory), alert: "コメントの投稿に失敗しました。"
-    # end
   end
 
   def destroy
     @memory = Memory.find(params[:memory_id])
     @memory_comments = @memory.memory_comments.order("created_at DESC")
     @memory_comment = MemoryComment.find(params[:id])
-    @memory_comment.destroy
-    # redirect_to memory_path(@memory),  notice: "コメントを削除しました。"
+    if @memory_comment.end_user_id == current_end_user.id
+      @memory_comment.destroy
+    else
+      redirect_to memory_path(@memory), alert: '自分のメモリーの以外は編集できません'
+    end
   end
 
   private
+
 
   def memory_comment_params
     params.require(:memory_comment).permit(:comment, :memory_id, :end_user_id)
